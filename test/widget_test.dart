4e04b743000db8713628cdf5597ecef541315b42
EleverDhaivat <dhaivat.rawal@elever.app>
2022-11-03 09:04:51 +0530

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:PlayerApp/main.dart';
+import 'package:player_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
